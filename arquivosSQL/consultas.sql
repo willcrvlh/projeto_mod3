6072b49a13ddc68114f61171f22e2a4379df8122
Well <83258570+wellisonsales@users.noreply.github.com>
2022-06-26 19:04:53 -0300

    correção de português na pergunta

--- arquivosSQL/consultas.sql
+++ arquivosSQL/consultas.sql
@@ -1,4 +1,4 @@
--- qual a quantidade de aotes que tem a aparição super que "50" vezes?
+-- qual a quantidade de aotes que tem a aparição superior que "50" vezes?
 
 SELECT * FROM got.atores
 WHERE Qtd_Aparicao > 50;
