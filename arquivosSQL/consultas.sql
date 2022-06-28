e0f59c87fd1caf49c3a6881bc4cc32973782d186
Well <83258570+wellisonsales@users.noreply.github.com>
2022-06-28 00:31:18 -0300

    atualização de perguntas usando função de soma sql

--- arquivosSQL/consultas.sql
+++ arquivosSQL/consultas.sql
@@ -8,3 +8,11 @@ WHERE Qtd_Aparicao > 50;
 SELECT * FROM got.atores
 WHERE Qtd_Aparicao < 3;
 
+-- quantos episódios obtivemos na temporada 1?
+
+SELECT * FROM got.episodios
+WHERE temporada = "1" -- RESULTADO: a consulta revela que obtivermos 10 episódios.
+
+-- se somassemos todas as aparições de personagens, quantas vezes os personagens apareceram na tela?
+
+SELECT SUM(Qtd_Aparicao) FROM got.atores; -- RESULTADO: 3497 aparições em tela em todas as temporadas.
