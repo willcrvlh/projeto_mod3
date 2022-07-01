61dfabb8d3c8ae38e977fd94dcf629703582355c
Well <83258570+wellisonsales@users.noreply.github.com>
2022-07-01 20:14:41 -0300

    corrigindo sintaxe das minhas querys

--- consultas/consultas.sql
+++ consultas/consultas.sql
@@ -95,17 +95,17 @@ LIMIT 1;
 
 -- qual a quantidade de aotes que tem a aparição superior que "50" vezes?
 
-SELECT * FROM got.atores
+SELECT * FROM atores
 WHERE quantidade_aparicao > 50;
 
 -- qual atores menos aparecem em tela?
 
-SELECT * FROM got.atores
+SELECT * FROM atores
 WHERE quantidade_aparicao < 3;
 
 -- quantos episódios obtivemos na temporada 1?
 
-SELECT * FROM got.episodios
+SELECT * FROM episodios
 WHERE temporada = "1" -- RESULTADO: a consulta revela que obtivermos 10 episódios.
 
 -- se somassemos todas as aparições de personagens, quantas vezes os personagens apareceram na tela?
